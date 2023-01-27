7bad27b771319289b75b3ba7c117e36ad457f508
Domenico De Maria <dome.demaria@gmail.com>
2023-01-27 18:31:54 +0100

    modify devInfo component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,12 +4,12 @@ import PhotoDanilo from '../../assets/imgs/logo192.png'
 
 const About = () => {
     return (
-        <div className='flex flex-col items-center h-def w-8/12 bg-black-200 mx-auto my-12 rounded-lg'>
-            <DevInfo devName='Danilo Candida' git='' linkedin='' img={PhotoDanilo}/>
-            <DevInfo devName='Nicholas Pala' git='' linkedin='' photo=''/>
-            <DevInfo devName='Felice Di Pasquale' git='' linkedin='' photo=''/>
-            <DevInfo devName="Daniele Ugo D'Anna" git='' linkedin='' photo=''/>
-            <DevInfo devName="Domenico De Maria" git='' linkedin='' photo=''/>
+        <div className='flex flex-col items-center h-def w-8/12 bg-black-200 mx-auto my-12 rounded-lg '>
+            <DevInfo devName='Danilo Candida' git='' linkedin='' img={PhotoDanilo} position='flex-row' contentPosition='justify-start'/>
+            <DevInfo devName='Nicholas Pala' git='' linkedin='' photo='' position='flex-row-reverse' contentPosition='content-end'/>
+            <DevInfo devName='Felice Di Pasquale' git='' linkedin='' photo='' position='flex-row' contentPosition='content-start'/>
+            <DevInfo devName="Daniele Ugo D'Anna" git='' linkedin='' photo='' position='flex-row-reverse' contentPosition='content-end'/>
+            <DevInfo devName="Domenico De Maria" git='' linkedin='' photo='' position='flex-row' contentPosition='content-start'/>
 
         </div>
     )
--- src/components/about/devInfo/devInfo.js
+++ src/components/about/devInfo/devInfo.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 const DevInfo = (props) => {
-    return(
-        <div className='flex flex-row w-8/12 min-h-dev bg-white'>
+    return (
+        <div className={"flex w-8/12 min-h-dev bg-white "+ props.position}>
             <div className='bg-red-100 w-1/4 min-h-full'>
                 <img src={props.img} alt=''></img>
             </div>
-            <div className='flex flex-col items-center bg-green-100 w-3/4 min-h-full '>
-                <p className='text-teal-100 my-2'>{props.devName}</p>
+            <div className={'flex flex-col bg-green-100 w-3/4 min-h-full '+ props.contentPosition}>
+                <p className='text-teal-100 my-2 '>{props.devName}</p>
                 <div className='h-0.5 w-4/12 bg-teal-100 my-1'></div>
+                <div className='flex space-x-2 w-8/12 h-4'>
+                    <div className='flex w-1/2 h-4'>
+                        <div className='w-1/4 h-4 bg-teal-200'></div>
+                        <div className='w-3/4 h-4 bg-red-100'></div>
+                    </div>
+                    <div className='flex w-1/2 h-4'>
+                        <div className='w-1/4 h-4 bg-teal-200'></div>
+                        <div className='w-3/4 h-4 bg-red-100'></div>
+                    </div>
+
+                </div>
             </div>
         </div>
 
