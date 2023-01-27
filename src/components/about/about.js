ae8d5f166cbd6f82f24c4ca2eed48e2c41a8e25c
Felice <Felixe70@gmail.com>
2023-01-27 17:31:39 +0100

    modify devInfo component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import DevInfo from './devInfo/devInfo';
+import PhotoDanilo from '../../assets/imgs/logo192.png'
 
 const About = () => {
     return (
         <div className='flex flex-col items-center h-def w-8/12 bg-black-200 mx-auto my-12 rounded-lg'>
-            <DevInfo />
+            <DevInfo devName='Danilo Candida' git='' linkedin='' img={PhotoDanilo}/>
+            <DevInfo devName='Nicholas Pala' git='' linkedin='' photo=''/>
+            <DevInfo devName='Felice Di Pasquale' git='' linkedin='' photo=''/>
+            <DevInfo devName="Daniele Ugo D'Anna" git='' linkedin='' photo=''/>
+            <DevInfo devName="Domenico De Maria" git='' linkedin='' photo=''/>
 
         </div>
     )
--- src/components/about/devInfo/devInfo.js
+++ src/components/about/devInfo/devInfo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const DevInfo = () => {
+const DevInfo = (props) => {
     return(
         <div className='flex flex-row w-8/12 min-h-dev bg-white'>
-            <div className='bg-red-100 w-1/4 min-h-full'></div>
-            <div className='bg-green-100 w-3/4 min-h-full'></div>
+            <div className='bg-red-100 w-1/4 min-h-full'>
+                <img src={props.img} alt=''></img>
+            </div>
+            <div className='flex flex-col items-center bg-green-100 w-3/4 min-h-full '>
+                <p className='text-teal-100 my-2'>{props.devName}</p>
+                <div className='h-0.5 w-4/12 bg-teal-100 my-1'></div>
+            </div>
         </div>
 
     )
