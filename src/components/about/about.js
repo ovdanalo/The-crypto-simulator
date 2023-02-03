4c826ffd0af7bed78f5c8791469c8d1c079107a5
Felice <Felixe70@gmail.com>
2023-02-03 11:59:08 +0100

    feat: add component banner

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,15 +2,19 @@ import React from 'react';
 import DevInfo from './devInfo/devInfo';
 import PhotoDanilo from '../../assets/imgs/logo192.png'
 import coin from '../../assets/imgs/coinGeckoLogo.png'
+import Banner from './banner/banner';
+import ImgDevelhope from '../../assets/imgs/Develhope.png'
+
 
 const About = () => {
     return (
         <div className='flex flex-col items-center h-def w-8/12 bg-black-200 mx-auto my-12 rounded-lg '>
-            <DevInfo devName='Danilo Candida' git='https://github.com/ovdanalo' linkedin='' img={PhotoDanilo} align='items-start' position='flex-row' linkPos='justify-start' roundedImg='rounded-l-full' roundedDiv='rounded-r-full'/>
-            <DevInfo devName='Nicholas Pala' git='' linkedin='' img={coin} align='items-end' position='flex-row-reverse' linkPos='justify-end' roundedImg='rounded-r-full' roundedDiv='rounded-l-full'/>
-            <DevInfo devName='Felice Di Pasquale' git='' linkedin='' img={PhotoDanilo} align='items-start' position='flex-row' linkPos='justify-start' roundedImg='rounded-l-full' roundedDiv='rounded-r-full'/>
-            <DevInfo devName="Daniele Ugo D'Anna" git='' linkedin='' img={PhotoDanilo} align='items-end' position='flex-row-reverse' linkPos='justify-end' roundedImg='rounded-r-full' roundedDiv='rounded-l-full'/>
-            <DevInfo devName="Domenico De Maria" git='' linkedin='' img={PhotoDanilo} align='items-start' position='flex-row' linkPos='justify-start' roundedImg='rounded-l-full' roundedDiv='rounded-r-full'/>
+            <Banner img={ImgDevelhope}/>
+            <DevInfo devName='Danilo Candida' git='https://github.com/ovdanalo' linkedin='https://www.linkedin.com/in/danilo-candida-1ba473210/' img={PhotoDanilo} align='items-start' position='flex-row' linkPos='justify-start' roundedImg='rounded-l-full' roundedDiv='rounded-r-full'/>
+            <DevInfo devName='Nicholas Pala' git='https://github.com/nickstershovel' linkedin='' img={coin} align='items-end' position='flex-row-reverse' linkPos='justify-end' roundedImg='rounded-r-full' roundedDiv='rounded-l-full'/>
+            <DevInfo devName='Felice Di Pasquale' git='https://github.com/felixe88' linkedin='' img={PhotoDanilo} align='items-start' position='flex-row' linkPos='justify-start' roundedImg='rounded-l-full' roundedDiv='rounded-r-full'/>
+            <DevInfo devName="Daniele Ugo D'Anna" git='https://github.com/Nelexiad' linkedin='' img={PhotoDanilo} align='items-end' position='flex-row-reverse' linkPos='justify-end' roundedImg='rounded-r-full' roundedDiv='rounded-l-full'/>
+            <DevInfo devName="Domenico De Maria" git='https://github.com/domedema' linkedin='' img={PhotoDanilo} align='items-start' position='flex-row' linkPos='justify-start' roundedImg='rounded-l-full' roundedDiv='rounded-r-full'/>
 
         </div>
     )
