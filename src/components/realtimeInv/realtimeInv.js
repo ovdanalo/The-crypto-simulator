1c3b7302c1cc37a9ba69a606a6ba7f24a6fd499a
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2023-03-07 10:49:58 +0100

    fix responsive realtime inv

--- src/components/realtimeInv/realtimeInv.js
+++ src/components/realtimeInv/realtimeInv.js
@@ -7,6 +7,7 @@ const Realtime = ({ data }) => {
     euroValue: "",
     selectedCrypto: "bitcoin",
   });
+  const [showAdd, setShowAdd] = useState(false)
 
   useEffect(() => {
     const storedData = localStorage.getItem("cryptoData");
@@ -48,26 +49,35 @@ const Realtime = ({ data }) => {
     const { selectedCrypto, euroValue } = inputData;
     const selectedCryptoData = data.find((item) => item.id === selectedCrypto);
     let crAm =
-    (updatedData[selectedCrypto]?.crAm || 0) +
-    parseFloat(euroValue) / selectedCryptoData.current_price;
+      (updatedData[selectedCrypto]?.crAm || 0) +
+      parseFloat(euroValue) / selectedCryptoData.current_price;
     const moAmEur = crAm * selectedCryptoData.current_price;
     const tetherData = data.find((item) => item.id === "tether");
     let moAmUsd = moAmEur * 1 / tetherData.current_price;
     const priceChange = selectedCryptoData.price_change_percentage_7d_in_currency;
     if (currency === 'USD') {
       crAm = (updatedData[selectedCrypto]?.crAm || 0) +
-      parseFloat(euroValue) * tetherData.current_price / selectedCryptoData.current_price;
+        parseFloat(euroValue) * tetherData.current_price / selectedCryptoData.current_price;
       moAmUsd = crAm * selectedCryptoData.current_price * (1 / tetherData.current_price);
     }
-    
+
     updatedData[selectedCrypto] = { moAmEur, moAmUsd, crAm, priceChange };
     localStorage.setItem("cryptoData", JSON.stringify(updatedData));
     setCryptoData(updatedData);
+    setShowAdd(false)
   };
 
+  const addButton = () => {
+    setShowAdd(true)
+  }
+
+  const cancelButton = () => {
+    setShowAdd(false);
+  }
+
   return (
-    <div className='flex flex-row bg-black-200 w-8/12 xl:h-def mx-auto my-6 justify-center rounded-lg'>
-      <div className='flex flex-col bg-black-100 w-1/3 m-6 mr-0 rounded-lg py-12'>
+    <div className='flex flex-row bg-black-200 w-full md:w-10/12 xl:w-8/12 h-def mx-auto my-6 justify-center rounded-lg'>
+      <div className='xl:flex flex-col bg-black-100 w-1/3 m-6 mr-0 rounded-lg py-12 hidden'>
         <form>
           <div>
             <label for='amount' className='flex text-white py-6 justify-center'>
@@ -77,12 +87,12 @@ const Realtime = ({ data }) => {
               name='amount'
               id='inputAmount'
               type='number'
-              className='rounded-tl-lg rounded-bl-lg text-center p-1'
+              className='rounded-tl-lg rounded-bl-lg text-center p-1 bg-teal-100 shadow'
               onChange={(e) =>
                 setInputData({ ...inputData, euroValue: e.target.value })
               }
             ></input>
-            <select id='money' onChange={(event) => setCurrency(event.target.value)} className='rounded-tr-lg rounded-br-lg p-1'>
+            <select id='money' onChange={(event) => setCurrency(event.target.value)} className='rounded-tr-lg rounded-br-lg p-1 bg-teal-100 shadow'>
               <option>EUR</option>
               <option>USD</option>
             </select>
@@ -101,7 +111,7 @@ const Realtime = ({ data }) => {
               name='crypto'
               size='27'
               autocomplete='off'
-              className='rounded-lg p-1'
+              className='rounded-lg p-1 bg-teal-100 shadow'
               onChange={(e) =>
                 setInputData({ ...inputData, selectedCrypto: e.target.value })
               }
@@ -110,13 +120,13 @@ const Realtime = ({ data }) => {
 
           <button
             onClick={handleSave}
-            class='bg-teal-300 hover:bg-teal-200 text-white font-bold py-3 px-16 mt-16 rounded-full'
+            class='bg-teal-300 hover:bg-teal-200 text-white font-bold py-3 px-16 mt-16 rounded-lg shadow'
           >
             BUY
           </button>
         </form>
       </div>
-      <div className='flex flex-col bg-black-100 w-2/3 m-6 rounded-lg'>
+      <div className='flex flex-col bg-black-100 w-full xl:w-2/3 m-6 rounded-lg'>
         {cryptoData && Object.keys(cryptoData).length > 0 && (
           <table>
             <thead>
@@ -222,6 +232,63 @@ const Realtime = ({ data }) => {
             </tbody>
           </table>
         )}
+        {showAdd && <div className='mt-10 p-10 self-center flex-col bg-black-200 w-65 rounded-lg xl:hidden z-10 border-solid border-2 border-teal-200 absolute'>
+
+          <form>
+            <div>
+              <label for='amount' className='flex text-white py-6 justify-center'>
+                How much do you want to invest?
+              </label>
+              <input
+                name='amount'
+                id='inputAmount'
+                type='number'
+                className='rounded-tl-lg rounded-bl-lg text-center p-1 bg-teal-100 shadow'
+                onChange={(e) =>
+                  setInputData({ ...inputData, euroValue: e.target.value })
+                }
+              ></input>
+              <select id='money' onChange={(event) => setCurrency(event.target.value)} className='rounded-tr-lg rounded-br-lg p-1 bg-teal-100 shadow'>
+                <option>EUR</option>
+                <option>USD</option>
+              </select>
+            </div>
+            <div>
+              <label
+                for='crypto'
+                className='flex text-white pt-12 pb-6 justify-center '
+              >
+                In which cryptocurrency?
+              </label>
+
+              <input
+                list='cryptoList'
+                id='selectCrypto'
+                name='crypto'
+                size='27'
+                autocomplete='off'
+                className='rounded-lg p-1 bg-teal-100 shadow'
+                onChange={(e) =>
+                  setInputData({ ...inputData, selectedCrypto: e.target.value })
+                }
+              />
+            </div>
+            <div className='flex'>
+              <button
+                onClick={handleSave}
+                class='bg-teal-300 hover:bg-teal-200 text-white font-bold py-3 px-10 m-6 mt-16 rounded-lg shadow'
+              >
+                BUY
+              </button>
+              <button onClick={cancelButton} class='bg-red-200 hover:bg-red-300 text-white font-bold py-3 px-8 m-6 mt-16 rounded-lg shadow'>
+                CANCEL
+              </button>
+            </div>
+          </form>
+        </div>}
+        <button onClick={addButton} class="self-center bg-teal-300 hover:bg-teal-200 text-white font-bold w-3 py-3 px-12 mt-12 mb-12 rounded-lg shadow flex justify-center align-middle  xl:hidden ">
+          ADD
+        </button>
       </div>
       <datalist id='cryptoList'>
         {data.map((crypto) => (
